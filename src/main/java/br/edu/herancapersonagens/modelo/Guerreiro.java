a4bb7acc2f361eca7b9512649ad92e7e61b4b307
gabriel Linux <carmogabriel0429@gmail.com>
2025-09-21 20:37:45 -0300

    criação da Classe mãe "Guerreiro" e estruturação da classe "Main"

--- src/main/java/br/edu/herancapersonagens/modelo/Guerreiro.java
+++ src/main/java/br/edu/herancapersonagens/modelo/Guerreiro.java
@@ -1,1 +1,49 @@
+package modelo;
 
+import java.security.PublicKey;
+
+public abstract class Guerreiro{
+    private final String nome;
+    private String poder;
+    private int vida = 100;
+    private int dano;
+    private String classe;
+
+    //-------------- constructor e acessores
+
+    public Guerreiro(String nome, String poder, int dano, String classe) {
+        this.nome = nome;
+        this.poder = poder;
+        this.dano = dano;
+        this.classe = classe;
+    }
+    //--------------------- PODER
+    public String getPoder() {
+        return poder;
+    }
+    public void setPoder(String poder) {
+        this.poder = poder;
+    }
+    //--------------------- VIDA
+    public int getVida() {
+        return vida;
+    }
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    //----------------------DANO --------
+    public int getDano() {
+        return dano;
+    }
+    public void setDano(int dano) {
+        this.dano = dano;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    //------------------------ ESPECIAIS
+
+    public abstract void atacar();
+}
